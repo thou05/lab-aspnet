3ac6ca5f52f4dc74ab01fddab2fab1fd0a7fd979
thou05 <thaao.lt@gmail.com>
2025-08-27 23:44:52 +0700

    add lab day2

---
+++ day02-controller/day02-controller/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace day02_controller.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Title = "Product List";
+            ViewBag.Products = new List<string> { "Product A", "Product B", "Product C" };
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewBag.Title = "Product Details";
+            ViewBag.ProductName = "Laptop Dell XPS 13";
+            ViewBag.Price = 30000000;
+            
+            return View();
+        }
+    }
+}
---
+++ day02-controller/day02-controller/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace day02_controller.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            ViewBag.Title = "User Profile";
+            ViewBag.Username = "thaole";
+            ViewBag.Email = "thao@gmail.com";
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            ViewBag.Title = "User Registration";
+            ViewBag.Message = "Please fill in the form to register a new account";
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return RedirectToAction("Profile");
+        }
+
+        public IActionResult Logout()
+        {
+            ViewBag.Message = "You have successfully logged out";
+            return RedirectToAction("List", "Product");
+        }
+    }
+}
